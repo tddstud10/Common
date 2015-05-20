770b62db430244c22d38117c7fef37a3402a8190
Partho P. Das <parthopdas@live.com>
2015-05-20 23:29:04 +1000

    some fixes.

---
+++ Domain/RunDomain.fs
@@ -0,0 +1,50 @@
+﻿namespace R4nd0mApps.TddStud10.Common.Domain
+
+open System
+open System.Collections.Generic
+open Microsoft.VisualStudio.TestPlatform.ObjectModel
+
+type PerAssemblyTestCases = IReadOnlyDictionary<FilePath, TestCase seq>
+
+type RunData = 
+    { startTime : DateTime
+      solutionPath : FilePath
+      solutionSnapshotPath : FilePath
+      solutionBuildRoot : FilePath
+      testsPerAssembly : PerAssemblyTestCases 
+      sequencePoints : PerDocumentSequencePoints option
+      codeCoverageResults : PerAssemblySequencePointsCoverage option
+      executedTests : PerTestIdResults option }
+
+
+type RunStepResult = 
+    { name : RunStepName
+      kind : RunStepKind
+      status : RunStepStatus
+      addendum : RunStepStatusAddendum
+      runData : RunData }
+
+exception RunStepFailedException of RunStepResult
+
+type RunStepEventArg = 
+    { name : RunStepName
+      kind : RunStepKind
+      runData : RunData }
+
+type RunStepEndEventArg = RunStepResult
+
+type RunStepEvents = 
+    { onStart : Event<RunStepEventArg>
+      onError : Event<RunStepEndEventArg>
+      onFinish : Event<RunStepEndEventArg> }
+
+type RunStepFunc = IRunExecutorHost -> RunStepName -> RunStepKind -> RunStepEvents -> RunData -> RunStepResult
+
+type RunStepFuncWrapper = RunStepFunc -> RunStepFunc
+
+type RunStep = 
+    { name : RunStepName
+      kind : RunStepKind
+      func : RunStepFunc }
+
+type RunSteps = RunStep array
