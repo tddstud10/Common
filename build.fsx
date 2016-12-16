ff5610eab729b6e6af00e05d1beac54c94aeb58e
Partho P. Das <this@parthopdas.com>
2016-12-16 23:14:30 +1100

    Integrating GitLink

--- build.fsx
+++ build.fsx
@@ -9,6 +9,7 @@ open System.IO
 MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some MSBuildVerbosity.Minimal }
 
 // Directories
+let packagesDir = "./packages/"
 let buildDir  = @"./build/"
 let testDir  = @"./build/"
 let nugetDir = @"./NuGet/"
@@ -30,11 +31,26 @@ Target "Build" (fun _ ->
          [
             "Configuration", "Debug"
             "Platform", "Any CPU"
-            "CreateVsixContainer", "true"
-            "DeployExtension", "false"
-            "CopyVsixExtensionFiles", "false"
          ]
     |> ignore
+)
+
+Target "GitLink" (fun _ ->
+    let gitLink = (packagesDir @@ @"gitlink" @@ "lib" @@ "net45" @@ "GitLink.exe")
+    let args = sprintf "%s -f %s -d %s" __SOURCE_DIRECTORY__ solutionFile buildDir
+    let ret =
+        ExecProcessAndReturnMessages (fun info ->
+            info.FileName <- gitLink
+            info.Arguments <- args) (TimeSpan.FromSeconds 30.0)
+    let consoleOutput =
+        ret.Messages
+        |> Seq.append ret.Errors
+    consoleOutput
+    |> Seq.iter (printfn "%s")
+    let loadFailures =
+        consoleOutput
+        |> Seq.filter (fun m -> m.ToLowerInvariant().Contains("failed to load project"))
+    if not ret.OK || not (Seq.isEmpty loadFailures) then failwith (sprintf "GitLink.exe \"%s\" task failed.\nErrors:\m %A" args loadFailures)
 )
 
 let runTest pattern =
@@ -58,7 +74,7 @@ Target "Package" (fun _ ->
             Project = "TddStud10.Common"
             Description = "TddStud10 Common"
             Version = EnvironmentHelper.environVarOrDefault "GitVersion_NuGetVersion" "0.0.0"
-            Dependencies = [ "FSharp.Core", GetPackageVersion "./packages/" "FSharp.Core" ]
+            Dependencies = [ "FSharp.Core", GetPackageVersion packagesDir "FSharp.Core" ]
             Files = [ buildDirRel "R4nd0mApps.TddStud10.Common.Domain.dll", Some "lib", None
                       buildDirRel "R4nd0mApps.TddStud10.Common.Domain.pdb", Some "lib", None
                       buildDirRel "R4nd0mApps.TddStud10.Logger.dll", Some "lib", None
@@ -76,6 +92,8 @@ Target "Publish" (fun _ ->
 "Build" ==> "Rebuild" 
 "Build" ?=> "UnitTests" ==> "Test"
 "Rebuild" ==> "Test"
+"GitLink" ==> "Package"
+"Test" ?=> "GitLink"
 "Test" ==> "Package" ==> "Publish"
 
 // start build
