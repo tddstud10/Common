377508ec63f04d910d2837860ea08bdd43f0ea43
Partho P. Das <this@parthopdas.com>
2016-12-16 14:12:16 +1100

    Integrating gitversion

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-ci.66+Branch.master.Sha.7fedc7ff52738f800aa268f0b88ca9ce453883b6")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ AssemblyInfo.fs
@@ -0,0 +1,11 @@
+#nowarn "2003"
+
+namespace R4nd0mApps.TddStud10
+
+open System.Reflection
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyInformationalVersion("0.1.0-ci.66+Branch.master.Sha.7fedc7ff52738f800aa268f0b88ca9ce453883b6")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do()
