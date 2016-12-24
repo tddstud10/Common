3a2ee192eed317c7c3d365e0f5f3450a57d1eec0
Partho P. Das <parthopdas@live.com>
2016-12-24 13:16:04 +1100

    Removing constants.* and adding isDF

---
+++ Domain/Code/DFizer.fs
@@ -0,0 +1,6 @@
+﻿module R4nd0mApps.TddStud10.Common.DFizer
+
+open System
+open System.Reflection
+
+let isDF() = Assembly.GetExecutingAssembly().GetName().Name.EndsWith(".DF", StringComparison.Ordinal)
