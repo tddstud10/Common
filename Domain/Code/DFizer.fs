0780ab9719ee6146c45dbef759a6b1218d28d071
Partho P. Das <parthopdas@live.com>
2017-06-02 21:19:07 +1000

    dfizing paths

--- Domain/Code/DFizer.fs
+++ Domain/Code/DFizer.fs
@@ -1,8 +1,15 @@
 ﻿module R4nd0mApps.TddStud10.Common.DFizer
 
 open System
+open System.IO
 open System.Reflection
 
 let isDF() = Assembly.GetExecutingAssembly().GetName().Name.EndsWith(".DF", StringComparison.Ordinal)
 
 let dfize s = sprintf "%s%s" s (if isDF() then ".DF" else "")
+
+let dfizePath path = 
+    let newExtn = 
+        sprintf "%s%s" (if isDF() then ".DF"
+                        else "") (Path.GetExtension path)
+    (path, newExtn) |> Path.ChangeExtension
